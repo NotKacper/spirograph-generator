13f007b8f85fc5ff20518b2e9ee361a361c0b590
Kacper Piotr Lubczynski <kacperlubczynski564@gmail.com>
2023-11-05 23:44:09 +0000

    Fixed script

--- src/SpirographApp.java
+++ src/SpirographApp.java
@@ -1,18 +1,5 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class SpirographApp extends Application {
-
-	@Override
-	public void start(Stage stage) throws Exception {
-		stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("paint.fxml"))));
-		stage.setTitle("Spirograph plotter");
-		stage.show();
-	}
-
+public class SpirographApp {
 	public static void main(String[] args) {
-		launch(args);
+		SpirographApp.main(args);
 	}
 }
