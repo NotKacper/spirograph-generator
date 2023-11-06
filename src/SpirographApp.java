ba579ac6f35b73639063d5b30a1326c165193464
Kacper Lubczynski <kacperlubczynski564@gmail.com>
2023-11-06 00:07:13 +0000

    Fixed stuff

--- src/SpirographApp.java
+++ src/SpirographApp.java
@@ -1,5 +1,22 @@
-public class SpirographApp {
-	public static void main(String[] args) {
-		SpirographApp.main(args);
-	}
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
+public class SpirographApp extends Application {
+
+  @Override
+  public void start(Stage stage) throws IOException {
+    stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("paint.fxml"))));
+    stage.setTitle("Spirograph Plotter");
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
